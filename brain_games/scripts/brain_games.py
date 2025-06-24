3015a101d3f88fe2e7d654d45fe35fbf20ed4f03
dayanunix <dayanuber35@gmail.com>
2025-06-24 09:48:32 -0500

    todo listo

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,9 +2,16 @@
 
 from brain_games.cli import welcome_user
 from brain_games.scripts.brain_even import first_question
+from brain_games.scripts.brain_calc import calculate
+from brain_games.scripts.brain_gcd import mcd
+from brain_games.scripts.brain_progression import progression
 def main():
     welcome_user()
     first_question()
+    calculate()
+    mcd()
+    progression()
+
 
 if __name__ == "__main__":
     main()

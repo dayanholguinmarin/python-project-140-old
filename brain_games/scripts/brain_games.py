51ee3bac45b7324679e5259b20e2ead9e5554c8d
dayan holguin marin <113401319+dayanholguinmarin@users.noreply.github.com>
2025-10-29 18:19:16 -0500

    commit welcome
    otro welcome

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
-from brain_games.scripts.brain_even import first_question
-from brain_games.scripts.brain_calc import calculate
-from brain_games.scripts.brain_gcd import mcd
-from brain_games.scripts.brain_progression import progression
-from brain_games.scripts.brain_prime import is_prime
-def main(name):
-    first_question(name)
-    calculate(name)
-    mcd(name)
-    progression(name)
-    is_prime(name)
+from brain_games.cli import welcome_user
 
+def main():
+    welcome_user()
 
 if __name__ == "__main__":
     main()
